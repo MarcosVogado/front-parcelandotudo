d445ab68a1a785bf4278858793bd64485baca03f
Marcos Vogado <marcoshcvogado@gmail.com>
2025-12-08 16:47:57 -0300

    Config burguer menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  isMenuOpen = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    if (window.innerWidth > 900 && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
 }
